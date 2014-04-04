0607f056c6b983a460d93b6a5dd8e7cc57f3aa3a
eXavera <stanislav@fajfr.cz>
2014-04-04 13:58:13 +0200

    Added an extension method for loading a DBF table into a DataTable.

---
+++ NDbfReader.Tests/TableExtensionsTests.cs
@@ -0,0 +1,103 @@
+﻿using FluentAssertions;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Xunit.Extensions;
+
+namespace NDbfReader.Tests
+{
+    public sealed class TableExtensionsTests
+    {
+        [Fact]
+        public void AsDataTable_ReturnsDataTableWithSchemaThatMatchesDbfTableColumns()
+        {
+            // Arrange
+            using (var table = Samples.OpenBasicTable())
+            {
+                var expectedDataTableColumns = Samples.BasicTableContent.Select(pair => new { Name = pair.Key, Type = pair.Value[0].GetType() });
+
+                // Act
+                var dataTable = table.AsDataTable();
+
+                // Assert
+                var actualDataTableColumns = dataTable.Columns.Cast<DataColumn>().Select(column => new { Name = column.ColumnName, Type = column.DataType });
+                actualDataTableColumns.ShouldAllBeEquivalentTo(expectedDataTableColumns);
+            }
+        }
+
+        [Theory]
+        [InlineData("TEXT")]
+        [InlineData("NUMERIC")]
+        [InlineData("LOGICAL")]
+        [InlineData("DATE")]
+        [InlineData("LONG")]
+        public void AsDataTable_ReturnsPopulatedDataTable(string columnName)
+        {
+            // Arrange
+            using (var table = Samples.OpenBasicTable())
+            {
+                var expectedValues = ReplaceNullWithDBNull(Samples.BasicTableContent[columnName]);
+
+                // Act
+                var dataTable = table.AsDataTable();
+
+                // Assert
+                var actualValues = dataTable.AsEnumerable().Select(row => row[columnName]);
+                actualValues.ShouldAllBeEquivalentTo(expectedValues);
+            }
+        }
+
+        [Fact]
+        public void AsDataTable_NoParameters_OpensReaderWithASCIIEncoding()
+        {
+            // Arrange
+            using (var table = GetMockedBasicSampleTable())
+            {
+                // Act
+                table.AsDataTable();
+
+                // Assert
+                table.Received().OpenReader(Encoding.ASCII);
+            }
+        }
+
+        [Fact]
+        public void AsDataTable_CustomEncoding_OpensReaderWithTheEncoding()
+        {
+            // Arrange
+            using (var table = GetMockedBasicSampleTable())
+            {
+                var encoding = Encoding.UTF8;
+
+                // Act
+                table.AsDataTable(encoding);
+
+                // Assert
+                table.Received().OpenReader(encoding);
+            }
+        }
+
+        private static IEnumerable<object> ReplaceNullWithDBNull(IEnumerable<object> items)
+        {
+            foreach (var item in items)
+            {
+                yield return item ?? DBNull.Value;
+            }
+        }
+
+        private static Table GetMockedBasicSampleTable()
+        {
+            return Substitute.ForPartsOf<MockableTable>(new Header(new List<IColumn>(), 0, 0), new BinaryReader(Samples.GetBasicTableStream()));
+        }
+
+        public class MockableTable : Table
+        {
+            public MockableTable(Header header, BinaryReader reader) : base(header, reader) { }
+        }
+    }
+}
---
+++ NDbfReader/TableExtensions.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Data;
+using System.Globalization;
+using System.Text;
+
+namespace NDbfReader
+{
+    /// <summary>
+    /// Extensions for for the <see cref="Table"/> class.
+    /// </summary>
+    public static class TableExtensions
+    {
+        /// <summary>
+        /// Loads the DBF table into a <see cref="DataTable"/> with the default ASCII encoding.
+        /// </summary>
+        /// <param name="table">The DBF table to load.</param>
+        /// <returns>A <see cref="DataTable"/> loaded from the DBF table.</returns>
+        /// <exception cref="InvalidOperationException">Another reader of the DBF table is opened.</exception>
+        /// <exception cref="ObjectDisposedException">The DBF table is disposed.</exception>
+        public static DataTable AsDataTable(this Table table)
+        {
+            if(table == null)
+            {
+                throw new ArgumentNullException("table");
+            }
+
+            var dataTable = CreateDataTable(table);
+            FillData(table, dataTable, table.OpenReader());
+
+            return dataTable;
+        }
+
+        /// <summary>
+        /// Loads the DBF table into a <see cref="DataTable"/>.
+        /// </summary>
+        /// <param name="table">The DBF table to load.</param>
+        /// <param name="encoding">The encoding that is used to load the rows content.</param>
+        /// <returns>A <see cref="DataTable"/> loaded from the DBF table.</returns>
+        /// <exception cref="InvalidOperationException">Another reader of the DBF table is opened.</exception>
+        /// <exception cref="ObjectDisposedException">The DBF table is disposed.</exception>
+        public static DataTable AsDataTable(this Table table, Encoding encoding)
+        {
+            if (table == null)
+            {
+                throw new ArgumentNullException("table");
+            }
+            if (encoding == null)
+            {
+                throw new ArgumentNullException("encoding");
+            }
+
+            var dataTable = CreateDataTable(table);
+            FillData(table, dataTable, table.OpenReader(encoding));
+
+            return dataTable;
+        }
+
+        private static DataTable CreateDataTable(Table table)
+        {
+            var dataTable = new DataTable()
+            {
+                Locale = CultureInfo.CurrentCulture
+            };
+
+            foreach (var column in table.Columns)
+            {
+                var columnType = Nullable.GetUnderlyingType(column.Type) ?? column.Type;
+                dataTable.Columns.Add(column.Name, columnType);
+            }
+
+            return dataTable;
+        }
+
+        private static void FillData(Table table, DataTable dataTable, Reader reader)
+        {
+            while (reader.Read())
+            {
+                var row = dataTable.NewRow();
+                foreach (var column in table.Columns)
+                {
+                    row[column.Name] = reader.GetValue(column) ?? DBNull.Value;
+                }
+
+                dataTable.Rows.Add(row);
+            }
+        }
+    }
+}
