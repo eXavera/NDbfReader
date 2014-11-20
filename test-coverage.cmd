319e1bfbf265fa9cb34f7752146e0c4310bf65b4
eXavera <stanislav@fajfr.cz>
2014-11-20 20:34:51 +0100

    Updated OpenCover and ReportGenerator packages.

--- test-coverage.cmd
+++ test-coverage.cmd
@@ -6,7 +6,7 @@ IF %ERRORLEVEL% GTR 0 EXIT /b
 RMDIR ".\opencover" /s /q
 
 MKDIR ".\opencover"
-.\packages\OpenCover.4.5.2506\OpenCover.Console.exe^
+.\packages\OpenCover.4.5.3207\OpenCover.Console.exe^
  -register:user^
  -target:".\packages\xunit.runners.1.9.2\tools\xunit.console.clr4.exe"^
  -targetargs:".\NDbfReader.Tests\bin\Debug\NDbfReader.Tests.dll /noshadow /silent"^
@@ -14,8 +14,8 @@ MKDIR ".\opencover"
  -output:.\opencover\output.xml
 
  MKDIR ".\opencover\report" /s /q
-.\packages\ReportGenerator.1.9.1.0\ReportGenerator.exe^
+.\packages\ReportGenerator.2.0.1.0\ReportGenerator.exe^
  -reports:.\opencover\output.xml^
  -targetdir:.\opencover\report^
- -reporttypes:Html,HtmlSummary
+ -reporttypes:Html
  .\opencover\report\index.htm
