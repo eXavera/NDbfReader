173d4c80a398b0178a2f95149be41d4863b00f4b
Stanislav Fajfr <stanislav@fajfr.cz>
2015-08-11 21:14:00 +0200

    Added NET 4.5 project.

---
+++ AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("NDbfReader")]
+[assembly: AssemblyDescription("DBF files reader for .NET Framework")]
+[assembly: AssemblyCompany("Stanislav Fajfr")]
+[assembly: AssemblyCopyright("Copyright © 2014 Stanislav Fajfr")]
+[assembly: AssemblyVersion("1.1.1.0")]
+[assembly: AssemblyFileVersion("1.1.1.0")]
--- NDbfReader.Net45/Properties/AssemblyInfo.cs
+++ NDbfReader.Net45/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyProduct("NDbfReader for .NET 4.0")]
+[assembly: AssemblyProduct("NDbfReader for .NET 4.5")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
