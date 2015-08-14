a997ad89433b54c165d413108c80da6796cb89d2
Stanislav Fajfr <stanislav@fajfr.cz>
2015-08-14 18:08:38 +0200

    Modified nuget package generation in order to support multiple targets.

--- build-nuget-package.cmd
+++ build-nuget-package.cmd
@@ -4,4 +4,4 @@ CALL .\build.cmd Release
 IF %ERRORLEVEL% GTR 0 EXIT /b
 
 IF NOT EXIST ".\dist" MKDIR ".\dist"
-.\.nuget\NuGet.exe pack ".\NDbfReader\NDbfReader.csproj" -OutputDirectory ".\dist" -Properties Configuration=Release
+.\.nuget\NuGet.exe pack -OutputDirectory ".\dist" -Properties Configuration=Release
