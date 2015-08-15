ef231285ab125d141d132c4db3ab700b42a4d63d
Stanislav Fajfr <stanislav@fajfr.cz>
2015-08-15 12:57:57 +0200

    Suppressed RECS0145 code analysis rule.

---
+++ GlobalSuppressions.cs
@@ -0,0 +1,7 @@
+
+// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Redundancies in Code", "RECS0145:Removes 'private' modifiers that are not required", Justification = "Explicity stating private helps in readability. It won't allow for a programmer to interpret its visibility differently.")]
