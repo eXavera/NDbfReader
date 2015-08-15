3011460ff0de128e4edfceb6eee59061d102f998
Stanislav Fajfr <stanislav@fajfr.cz>
2015-08-15 13:07:15 +0200

    Suppressed RECS0070 code analysis rule.

--- GlobalSuppressions.cs
+++ GlobalSuppressions.cs
@@ -6,4 +6,5 @@
 
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Redundancies in Code", "RECS0145:Removes 'private' modifiers that are not required", Justification = "Explicity stating private helps in readability. It won't allow for a programmer to interpret its visibility differently.")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Redundancies in Code", "RECS0129:Removes 'internal' modifiers that are not required", Justification = "Explicity stating internal helps in readability. It won't allow for a programmer to interpret its visibility differently.")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Redundancies in Code", "RECS0070", Justification = "Explicit parameter name helps in readability.")]
 
