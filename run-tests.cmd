8f1ea1926684b88f1dc9f673c81b40be2db527c6
Stanislav Fajfr <stanislav@fajfr.cz>
2015-11-07 18:39:19 +0100

    Solution level nuget packages moved to tests project.

--- run-tests.cmd
+++ run-tests.cmd
@@ -3,4 +3,4 @@
 CALL .\build.cmd
 IF %ERRORLEVEL% GTR 0 EXIT /b
 
-.\packages\xunit.runner.console.2.0.0\tools\xunit.console.exe .\NDbfReader.Tests\bin\Debug\NDbfReader.Tests.dll
+.\packages\xunit.runner.console.2.1.0\tools\xunit.console.exe .\NDbfReader.Tests\bin\Debug\NDbfReader.Tests.dll
--- test-coverage.cmd
+++ test-coverage.cmd
@@ -6,15 +6,15 @@ IF %ERRORLEVEL% GTR 0 EXIT /b
 RMDIR ".\opencover" /s /q
 
 MKDIR ".\opencover"
-.\packages\OpenCover.4.5.3723\OpenCover.Console.exe^
+.\packages\OpenCover.4.6.166\tools\OpenCover.Console.exe^
  -register:user^
- -target:".\packages\xunit.runner.console.2.0.0\tools\xunit.console.exe"^
+ -target:".\packages\xunit.runner.console.2.1.0\tools\xunit.console.exe"^
  -targetargs:".\NDbfReader.Tests\bin\Debug\NDbfReader.Tests.dll -noshadow -quiet"^
  -filter:+[NDbfReader]*^
  -output:.\opencover\output.xml
 
  MKDIR ".\opencover\report" /s /q
-.\packages\ReportGenerator.2.1.8.0\tools\ReportGenerator.exe^
+.\packages\ReportGenerator.2.3.4.0\tools\ReportGenerator.exe^
  -reports:.\opencover\output.xml^
  -targetdir:.\opencover\report^
  -reporttypes:Html
