4e3e14c0b87a03ff9b30fd02de9a8ccee980af44
Stanislav Fajfr <stanislav@fajfr.cz>
2015-11-07 18:19:53 +0100

    Removed redundant comma.

--- NDbfReader.Tests/Samples.cs
+++ NDbfReader.Tests/Samples.cs
@@ -13,7 +13,7 @@ namespace NDbfReader.Tests
             { "NUMERIC", new List<object> {123.123m, 456.456m, null} },
             { "LOGICAL", new List<object> {true, false, false} },
             { "DATE", new List<object> { new DateTime(2014, 2, 20), new DateTime(1998, 8, 15), null} },
-            { "LONG", new List<object> {  123456, -6544321, 0} },
+            { "LONG", new List<object> {  123456, -6544321, 0} }
         };
 
         public static Stream GetBasicTableStream()
