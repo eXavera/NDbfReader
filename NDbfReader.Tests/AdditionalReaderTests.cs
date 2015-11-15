f655012190c2b073ed9ff2f652a6bbd9ff13ee67
Stanislav Fajfr <stanislav@fajfr.cz>
2015-11-15 12:36:09 +0100

    Added async version of big czech sample test.

--- NDbfReader.Tests/AdditionalReaderTests.cs
+++ NDbfReader.Tests/AdditionalReaderTests.cs
@@ -1,21 +1,24 @@
 ﻿using System.Text;
+using System.Threading.Tasks;
+using NDbfReader.Tests.Infrastructure;
 using Xunit;
 
 namespace NDbfReader.Tests
 {
     public sealed class AdditionalReaderTests
     {
-        [Fact]
-        public void ReadsBigCzechSampleTableWithoutErrors()
+        [Theory]
+        [InlineDataWithExecMode]
+        public async Task ReadsBigCzechSampleTableWithoutErrors(bool useAsync)
         {
             // Arrange & Act
-            using (var table = Table.Open(EmbeddedSamples.GetStream(EmbeddedSamples.BIG_CZECH_DATA)))
+            using (var table = await this.Exec(() => Table.Open(EmbeddedSamples.GetStream(EmbeddedSamples.BIG_CZECH_DATA)), useAsync))
             {
                 var readRows = 0;
                 var reader = table.OpenReader(Encoding.GetEncoding(1250));
 
                 object value = null;
-                while (reader.Read())
+                while (await reader.Exec(r => r.Read(), useAsync))
                 {
                     foreach (var column in table.Columns)
                     {
