c6f6a1c9210572786a906b6e8571a9912341b7e3
Stanislav Fajfr <stanislav@fajfr.cz>
2016-07-08 22:12:35 +0200

    Updated nuget package and assembly info .

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -3,6 +3,6 @@
 [assembly: AssemblyTitle("NDbfReader")]
 [assembly: AssemblyDescription("DBF files reader for .NET Framework")]
 [assembly: AssemblyCompany("Stanislav Fajfr")]
-[assembly: AssemblyCopyright("Copyright © 2014 Stanislav Fajfr")]
+[assembly: AssemblyCopyright("Copyright © 2016 Stanislav Fajfr")]
 [assembly: AssemblyVersion("2.0.0.0")]
 [assembly: AssemblyFileVersion("2.0.0.0")]
