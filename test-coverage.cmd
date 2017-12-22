bca578e976c158248e62734e4f39d0c20f85f506
BBBB\sfajfr <stanislav.fajfr@blackboard.com>
2017-12-22 13:14:46 +0100

    Upgraded OpenCover and ReportGenerator nuget packages.

--- test-coverage.cmd
+++ test-coverage.cmd
@@ -6,7 +6,7 @@ IF %ERRORLEVEL% GTR 0 EXIT /b
 RMDIR ".\opencover" /s /q
 
 MKDIR ".\opencover"
-.\packages\OpenCover.4.6.166\tools\OpenCover.Console.exe^
+.\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe^
  -register:user^
  -target:".\packages\xunit.runner.console.2.3.1\tools\net452\xunit.console.exe"^
  -targetargs:".\NDbfReader.Tests\bin\Debug\NDbfReader.Tests.dll -noshadow -quiet"^
@@ -14,7 +14,7 @@ MKDIR ".\opencover"
  -output:.\opencover\output.xml
 
  MKDIR ".\opencover\report" /s /q
-.\packages\ReportGenerator.2.3.5.0\tools\ReportGenerator.exe^
+.\packages\ReportGenerator.3.1.1\tools\ReportGenerator.exe^
  -reports:.\opencover\output.xml^
  -targetdir:.\opencover\report^
  -reporttypes:Html
